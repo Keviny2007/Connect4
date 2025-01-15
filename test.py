aef80ff9edf5b7f07ad395c625a10943496bc5af
Keviny2007 <79064912+Keviny2007@users.noreply.github.com>
2025-01-14 16:48:41 -0800

    Remove file from tracking

--- test.py
+++
@@ -1,8 +0,0 @@
-from collections import namedtuple
-import numpy as np
-
-GameState = namedtuple('GameState', ['cur_state', 'board', 'cur_player'])
-initial_board = np.full((5, 5), '_', dtype=str)
-
-awei = GameState(cur_state='Ongoing', cur_player='X', board=initial_board)
-print(awei.cur_state)
